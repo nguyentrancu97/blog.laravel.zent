88a670cf8f3ede575a637a52241d5b7bf0176139
Nguyễn Trần Đức Cư <nguyentrancu97@gmail.com>
2021-07-16 15:30:33 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-       $schedule->command('schedule:test')->dailyAt('10:00');
+       $schedule->command('schedule:test')->dailyAt('15:35');
     }
 
     /**
